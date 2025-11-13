33ab9580a8d92e4fab025d21b2fd94ea8c257cef
pimp13 <pimp.puma.13@gmail.com>
2025-11-13 15:46:14 +0330

    Add Prisma integration and update dependencies

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
