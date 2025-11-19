257a1908f4b2698775e5a047e0cc317a36ff4a76
pimp13 <pimp.puma.13@gmail.com>
2025-11-19 16:34:18 +0330

    Refactor admin guard structure and introduce role-based access control
    - Updated import path for AdminGuard to align with new directory structure.
    - Added Roles decorator for defining user roles in the application.
    - Implemented AdminGuard to restrict access to admin routes based on user roles.
    - Introduced RolesGuard to enforce role-based access control across the application.

---
+++ src/common/guards/roles/roles.guard.ts
@@ -0,0 +1,35 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from 'src/common/decorators/roles/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+
+    if (!user) throw new ForbiddenException('You can not permission');
+
+    const userRoles = user.role || (user.role ? [user.role] : []);
+    const hasRole = requiredRoles.some((role) => userRoles.includes(role));
+
+    if (!hasRole)
+      throw new ForbiddenException('You can not permission for in work');
+
+    return true;
+  }
+}
